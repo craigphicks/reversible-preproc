a88d13a1b1ae3f6d3090a69c72e20136267ce6cc
craigphicks <13205732+craigphicks@users.noreply.github.com>
2019-09-04 13:51:06 -0700

    backup

---
+++ README.1.0.4.md
@@ -0,0 +1,203 @@
+XW# reversible-preproc 
+
+## 1. Outline
+
+This package is a library providing core functionality for a preprocessor, e.g., it can be used to implement preprocessing in a stream, or a CLI program. (For example, the *npm* package *reversible-preprocess-cli* is a CLI program implemented with this module -- *soon to be published* ).
+
+The *"reversible"* moniker indicates that it is suitable for lightweight switching back and forth between configuations (e.g., in-place).  The deactivated regions are marked with annotated comments (`//!!`) enabling the preprocessor to remove them for a different configuration. Repeated applications of the same `defines` are idempotent.
+
+Regexp is not used.  Neither is all text searched.  Only lines beginning with comment marks (e.g. `//`), are further processed as potential processing command lines.  This makes processing relatively fast. 
+
+
+## 2. interface
+
+The module exports a class `ReversiblePreproc` 
+
+```
+class ReversiblePreproc {
+    constructor(defines = {}, options = defaultOptions) {...}
+	line(line, callback = null) {...}
+}
+export default ReversiblePreproc
+```
+
+
+### 2.1 `constructor(defines, options)`
+
+#### 2.1.1 `defines`
+
+ - Normally `defines` is an object providing the property names and values to be referenced/tested by conditional statements embedded in the file/stream to be processed.
+ - As a special case the top level may the single single string `"*"`.  The effect is to evaluate 
+   all conditionals as true, i.e. nothing is commented out.
+ - The top level cannot be an array. Lower levels arrays are allowed.
+ - These top level property names are forbidden: `true, false, null, undefined, def, ndef, this`.
+ - Values are restricted to strings, numbers, and the special values `true`. `false`, and `null`. 
+   
+An example `defines`:
+
+```
+{
+	"DEBUG" : 2,
+	"select":1,
+	"configs": [{ 
+			'A':0,
+			'B':1
+		},{
+			'A':1,
+			'B':0,
+			'C':1
+	}],
+}
+```
+
+#### 2.1.2 `options`
+
+- `commentMark`: \<string\> [default `"//"`]
+  - only lines beginning with this commentMark will be further examined for preprocessor commands 
+  - reversibleCommentIndicator: <string> [default `"!!"`]
+ - the preprocessor disable a regions by prepending with `commentMark` followed by  `reversibleCommentIndicator`, e.g., `//!!` 
+- `testMode`: \<boolean\>  [default `false`] 
+	 - only output preprocess *command start lines*  prepended by `true` or `false`, according to the condtion evaluation.  
+ - `debugOutput`: 
+	 - prepend lines with the line number of the command condition whose range 
+ they are under, if any. (*Unlikely to be much use other than developer testing*)
+
+### 2.2  `line(line, callback = null) `
+This function performs stateful line by line processing
+#### 2.2.1 Return value
+ - When the passed argument `callback` is `null` it return a pair `[err, outputLine]`.  `err` 
+ will be null if there is no error to report.  Otherwise `err` will be an instance of `Error`, and the value of `outputline` is undetermined.
+ - When `callback` is not `null` it must be a function taking the the 
+ two arguments `err` and `outputline` in that order.
+
+
+#### 2.2.2. Stateful line processing logic
+
+The function to perform stateful preprocessing on each line of input.
+From the perspective of the preprocessor, input lines are categorized as follows:
+
+ - command start line
+ - command end line
+ - non-command line
+ 
+A **command start line** has the form
+```
+[commentMark][almost arbitrary fill]if<<CONDITIONAL-STATEMENT>>
+```
+For example, in the context of  the example defines written above:
+```
+//  if<<configs[select].A>>
+```
+A **command end line** has the same form but without `if` and the `CONDITIONAL-STATEMENT`.  E.g,
+```
+//  <<>>
+```
+
+The command end line marks the end of the range of the nearest command start line above it 
+which is not already matched by a command end line. 
+
+Hence, command start and end line pairs can be nested.  However, they cannot overlap.
+
+The form and action of the `CONDITIONAL-STATEMENT is explained in detail below.
+
+When the `CONDITIONAL-STATEMENT` evaluates to true, the **ordinary lines** in its range will be output without change.  However when it evaluates to false, the ordinary lines in its range will be prepended with the `commentMark` and `reversibleComment` indicator.  
+
+When commands are nested, the inner overrides the outer for the duration of its range.
+
+For example:
+
+*Before processing:*
+```
+// if<<true>>
+Mother duck and her chicks cross the road to get to the water. 
+John can 
+// if <<false>> 
+hardly wait to get home and can't 
+// <<>>
+stop the car in time.
+// <<>>
+```
+
+*After processing:*
+```
+// if<<true>>
+Mother duck and her chicks cross the road to get to the water. 
+John can 
+// if <<false>> 
+//!! hardly wait to get home and can't 
+// <<>>
+stop the car in time.
+// <<>>
+```
+
+### 2.3 `CONDITIONAL-STATEMENT`
+
+Two forms of conditional statement are enabled:
+
+ - **psuedo javascript** 
+	  - doesn't use javascript `eval`
+	  - parsed by [*jsep*](https://www.npmjs.com/package/jsep) into abstract parse tree form, then evaluated by a simple interpreter.
+ - **actual javascript**
+	  - uses javascript `eval`
+	  - client defined function is `eval`d with `defines` passed as argument.
+
+The **pseudo javascript** is more brief and readable, but the **actual javascript** is almighty.
+
+#### 2.3.1  Psuedo javascript conditional statement
+
+The 'pseudo javascript' uses the npm module 'jsep' to parse, and then an interpreter 
+to execute the parsed data. Example:
+```
+// if<<DEBUG>1 && configs[select].B>=1>>
+```
+which would evaluate to false using the above `defines` example.
+
+ - Round parentheses `(` and `)` are allowed for grouping logic.
+ - Square brackets `[` and `]` are allowed for property access as an alternative to `.` or where the identifier is a variable and `.` can't be used.
+- Allowed binary operators are: `<=` `<`  `>` `>=` `==` `===` `!=` `!==` `&&` `||`
+- Allowed unary operators are: `!`
+- Predefined keywords are: `true`, `false`, `null`, `undefined`, `def`, `ndef`
+
+Just like Javascript referencing a nonexistent property may or may not result in an exception.  
+(As explained above, exceptions will passed back in the result of the `line` function.)
+
+- Two predefined functions are provided:
+	 - `def(PROPERTY-EXPRESSION)` 
+	 - `ndef(PROPERTY-EXPRESSION)`
+    
+- `def()` evaluates to wither `true` or `false`, regardless of the property value.  
+- `true` is the result when no exception occurs and `PROPERTY-EXPRESSION` does not resolve to `undefined`
+- `false` is the result otherwise.
+- `def()` never results in an error value being passed back from `line()`.
+- `ndef()` simply returned the logical inverse of `def()`, i.e. `!def()`
+
+Example usage:
+```
+// if<< def(configs[select].C) && configs[select].C===1 >>
+```
+or
+```
+// if<< def(configs[2]) >> 
+```
+which are respectively `true` and `false` given the above `defines` example above.
+
+*Note: The [*jsep*](https://www.npmjs.com/package/jsep) module is not a dependency listed in `package.json` because it has been bundled directly into this module.*
+
+
+#### 2.3.2 Actual javascript conditional statement
+
+These have the form
+```
+:JAVASCRIPT-FUNCTION-OF-ONE-ARG
+```
+where `:` serves to differentiate from a **pseudo javascipt** statement.
+
+`JAVASCRIPT-FUNCTION-OF-ONE-ARG` will be `eval`d with `defines` passed as the arg.
+For example, alternates to the **pseudo javascipt** examples provided above:
+```
+// if<<:(D)=>{ return D.configs[select].C!==undefined && configs[select].C===1 } >>
+```
+or 
+```
+// if<<: (D)=>{ return D.configs.length > 2 }
+```
--- src/reversible-preproc.mjs
+++ src/reversible-preproc.mjs
@@ -3,196 +3,381 @@
 import JsepPreprocInterpret from './jsep-preproc-interpret.mjs'
 import RppBaseError from './prependable-error.mjs'
 
-var defaultOptions = {
-    testMode: false, // cmd start lines only prepended by true or false
-    debugOutput: false,
-    commentMark: '//',
-    reversibleCommentIndicator: '!!',
-}
-
-
 class RppError extends RppBaseError {
-    constructor(...params) {
-        super(...params)
-    }
+  constructor(...params) {
+    super(...params)
+  }
 
 }
+
+
+var defaultOptions = {
+  testMode: false, // cmd start lines only prepended by true or false
+  debugOutput: false,
+  // 1.x.x obsolete
+  commentMark: '//',
+  reversibleCommentIndicator: '!!',
+  // 2.x.x
+  cmdStem: '//--',
+  cmdIf: 'if',
+  cmdElse: 'else',
+  cmdElif: 'elif',
+  cmdEndif: 'endif',
+  cmdTpl: 'tpl',
+  cmdRender: 'render',
+  cmdTplRender: 'tplRender', // in a single line
+  cmdIfTplRender: 'ifTplRender', // in a single line
+  annPlain: 'plain',
+  annRendered: 'rendered',
+  annEndRendered: 'endRendered',
+  annStem: '//!!',
+}
+
+function getLineHeadSymbol(line, arrCmd, arrAnn) {
+  function testLineHead(line, start, str) {
+    return line.substr(start, str.length) === str
+  }
+  if (testLineHead(line, 0, opt.cmdStem)) {
+    //
+    let start = opt.cmdStem.length
+    for (item of arrCmd) {
+      if (testLineHead(line, start, item[0]))
+        return item[1]
+    }
+    throw new RppError(`no command found after stem in ${line}`)
+  } else if (testLineHead(line, 0, opt.annStem)) {
+    //
+    let start = opt.annStem.length
+    for (item of arrAnn) {
+      if (testLineHead(line, start, item[0]))
+        return item[1]
+    }
+    throw new RppError(`no annotation found after stem in ${line}`)
+  } else
+    return null // not command or annotated line
+}
+
 
 
 function judgeLineArg(str, definesJson, jsepPreprocInterpret) {
-    if (definesJson === '*') // this overrides and (and prevents calling of) any eval functions
-        return [true, null]
-    if (str[0] === ':') { // semicolon indicates to start a function to be eval'd
-        // the form is
-        // :(<variableName>)=>(<body>) and it be eval'd as a function
-        // ((<variableName>)=>(<body>))(<preproc jsonDefines>)
-        // where the preprocess json set up at program start will be passed as
-        // the argument to the function
-        try {
-            var fnstr = `(${str.slice(1)})(${JSON.stringify(definesJson)})`
-            return [eval(fnstr), null]
-        }
-        catch (e) {
-            return [
-                false,
-                new RppError(`function string ${fnstr} in ${str} could not be eval'd: `, e)
-            ]
-        }
-    } else {
-        // TODO jsepPreprocInterpret.lineScript(str) called from here.
-        try { return [jsepPreprocInterpret.execLineScript(str), null] }
-        catch (e) {
-            return [
-                false,
-                new RppError(
-                    `jsepPreprocInterpret failed on script :: ${str} :: `, e)
-            ]
-        }
+  if (definesJson === '*') // this overrides and (and prevents calling of) any eval functions
+    return [true, null]
+  if (str[0] === ':') { // semicolon indicates to start a function to be eval'd
+    // the form is
+    // :(<variableName>)=>(<body>) and it be eval'd as a function
+    // ((<variableName>)=>(<body>))(<preproc jsonDefines>)
+    // where the preprocess json set up at program start will be passed as
+    // the argument to the function
+    try {
+      var fnstr = `(${str.slice(1)})(${JSON.stringify(definesJson)})`
+      return [eval(fnstr), null]
     }
+    catch (e) {
+      return [
+        false,
+        new RppError(`function string ${fnstr} in ${str} could not be eval'd: `, e)
+      ]
+    }
+  } else {
+    // TODO jsepPreprocInterpret.lineScript(str) called from here.
+    try { return [jsepPreprocInterpret.execLineScript(str), null] }
+    catch (e) {
+      return [
+        false,
+        new RppError(
+          `jsepPreprocInterpret failed on script :: ${str} :: `, e)
+      ]
+    }
+  }
 }
 
 
 const reservedIdentifiers = [
-    'true', 'false', 'null', 'undefined', 'def', 'ndef'
+  'true', 'false', 'null', 'undefined', 'def', 'ndef'
 ]
 
+const symCmdIf = Symbol('cmdIf')
+const symCmdElse = Symbol('cmdElse')
+const symCmdElif = Symbol('cmdElif')
+const symCmdEndif = Symbol('cmdEndif')
+const symCmdTpl = Symbol('cmdTpl')
+const symCmdRender = Symbol('cmdRender')
+const symCmdTplRender = Symbol('cmdTplRender')
+const symCmdIfTplRender = Symbol('cmdIfTplRender')
+
+const symAnnPlain = Symbol('annPlain')
+const symAnnRendered = Symbol('annRendered')
+const symAnnEndRendered = Symbol('annEndRendered')
+
 class ReversiblePreproc {
-    constructor(definesJson = {}, options = defaultOptions) {
-        for (let k of Reflect.ownKeys(defaultOptions))
-            if (!Reflect.ownKeys(options).includes(k))
-                options[k] = defaultOptions[k]
-        this.options = options
-        if (!this.options.commentMark.length)
-            throw new RppError("options.commentMark cannot be zero length")
-        {
-            if (typeof definesJson === 'object') {
-                if (definesJson instanceof Array)
-                    throw new RppError("top level defined object cannot be an Array")
-                for (let k of Reflect.ownKeys(definesJson)) {
-                    if (reservedIdentifiers.includes(k)) {
-                        throw new RppError(`${k} is a reserved Identifier, cannot use it in top level defines`)
-                    }
-                }
-            }
-            this.definesJson = definesJson
+  constructor(definesJson = {}, options = defaultOptions) {
+    for (let k of Reflect.ownKeys(defaultOptions))
+      if (!Reflect.ownKeys(options).includes(k))
+        options[k] = defaultOptions[k]
+    this.options = options
+    if (!this.options.commentMark.length)
+      throw new RppError("options.commentMark cannot be zero length")
+    {
+      if (typeof definesJson === 'object') {
+        if (definesJson instanceof Array)
+          throw new RppError("top level defined object cannot be an Array")
+        for (let k of Reflect.ownKeys(definesJson)) {
+          if (reservedIdentifiers.includes(k)) {
+            throw new RppError(`${k} is a reserved Identifier, cannot use it in top level defines`)
+          }
         }
-        this.jsepPreprocInterpret = new JsepPreprocInterpret(definesJson)
-        this.linum = -1
-        this.onLinum = -1
-        this.on = 1
-        this.stack = []
-        Object.seal(this)
+      }
+      this.definesJson = definesJson
     }
-    _parseLine_aux(line) {
-        if (line.slice(0, 2) !== this.options.commentMark)
-            return [false, line, null]
-        let stripTarget = this.options.commentMark + this.options.reversibleCommentIndicator
-        // each line is always stripped of any previously inserted preproc comments
-        if (line.slice(0, stripTarget.length) === stripTarget) {
-            line = line.slice(stripTarget.length)
-            return [false, line, null]
-        }
-        let kind = { start: false, end: false }
-        let i0 = line.indexOf('if<<')
-        if (i0 == -1) {
-            i0 = line.indexOf('<<')
-            if (i0 == -1)
-                return [false, line, null]
-            i0 += 2
-            kind.end = true
+    this.jsepPreprocInterpret = new JsepPreprocInterpret(definesJson)
+    this.linum = -1
+    this.onLinum = -1
+    this.on = 1
+    this.stack = []
+    // 2.x.x 
+    this.parseState = {
+      linum: 0, // line number of the input file (count starts at 1)
+      ifOnLinum: -1, // the input line number of current innermost if block start
+      ifOn: false, // currently in an if blocks (possibly multiple)
+      stackIf: [], // stack for nested if blocks [ ..., [<startline>,<true/false>],...] 
+      renderedOn: false, // currently in an annotated rendered region, (will be cleared, maybe rerendered) 
+      tplString: null, // cmd-tpl lines are accumulated until cmd-render is encountered, thenoutput 
+    }
+    this.cmdsSorted = [
+      [options.cmdIf, symCmdIf],
+      [options.cmdElse, symCmdElse],
+      [options.cmdElif, symCmdElif],
+      [options.cmdEndif, symCmdEndif],
+      [options.cmdTpl, symCmdTpl],
+      [options.cmdRender, symCmdRender],
+      [options.cmdTplRender, symCmdTplRender],
+      [options.cmdIfTplRender, symCmdIfTplRender],
+    ]
+    this.annsSorted = [
+      [options.annPlain, symAnnPlain],
+      [options.annRendered, symAnnRendered],
+      [options.annEndRendered, symEndRendered],
+    ]
+    this.cmdsSorted.sort((a, b) => {
+      (a.length > b.length) ? 1 :
+        (a.length < b.length) ? -1 : 0
+    })
+    this.annsSorted.sort((a, b) => {
+      (a.length > b.length) ? 1 :
+        (a.length < b.length) ? -1 : 0
+    })
+
+
+
+    Object.seal(this)
+  }
+
+  _parseLine_aux2(line, state) { // can throw, returns [isCmd, strippedLine / null ]
+    // if ps 
+    let sym = getLineHeadSymbol(line, this.arrCmd, this.arrAnn)
+    if (!sym) {
+      if (this.parseState.renderedOn) 
+        return [false, null] // strip entire line -> null 
+      else
+        return [false, line]
+    } 
+    else if (sym === symAnnPlain) { 
+      // strip and return line w/out annotation
+      return [false, line.substr(
+        this.options.annStem.length + this.options.annPlain.length + 1)]
+    } 
+    else if (sym === symAnnRendered) {
+      if (this.parseState.renderedOn)
+        throw RppError('renderedOn is already true')
+      this.parseState.renderedOn = true
+      return [false, null]
+    } 
+    else if (sym === symAnnEndRendered) {
+      if (!this.parseState.renderedOn)
+        throw RppError('renderedOn is not true')
+      this.parseState.renderedOn = false
+      return [false, null]
+    } 
+
+
+  }
+  line2(line, callback = null) {
+    try {
+      this.parseState.linum++ // first line is line 1
+
+      let [isCmd, strippedLine, err] = this._parseLine_aux(line)
+
+
+      let dbg = ""
+      if (this.debugOutput)
+        dbg = `[${this.onLinum}, ${this.linum}]`
+      let [isCmd, strippedLine, err] = this._parseLine_aux(line)
+      if (isCmd) {
+        if (this.options.testMode) {
+          if (isCmd === 'start') {
+            return [
+              err,
+              dbg = (this.on ? 'true,  ' : 'false, ')
+              + `${strippedLine}`
+            ]
+          } else {
+            return [err, null]
+          }
         } else {
-            i0 += 4
-            kind.start = true
+          if (callback)
+            callback(err, (dbg + `${strippedLine}`))
+          else
+            return [err, (dbg + `${strippedLine}`)]
         }
-        let i1 = line.slice(i0).indexOf('>>')
-        if (i1 == -1)
-            return [false, line, null]
-        i1 += i0
-        let sub = line.slice(i0, i1)
-        if (kind.end) {
-            if (!this.stack.length) {
-                // too many end scope commnand - like unbalanced parentheses.
-                // Let's be strict.
-                let err = new RppError(`unexpected end directive line ${this.linum}, (unbalanced?)`)
-                return [true, line, err]
-            }
-            this.on = this.stack[this.stack.length - 1][0]
-            this.onLinum = this.stack[this.stack.length - 1][1]
-            this.stack.pop()
-            return ['end', line, null]
-        } else { // kind.start===true
-            if (!kind.start) throw new RppError("programming error")
-            let [isOn, err] = judgeLineArg(sub, this.definesJson, this.jsepPreprocInterpret)
-            this.stack.push([this.on, this.onLinum]) // save the previous state
-            this.on = isOn
-            this.onLinum = this.linum
-            return ['start', line, err]
+      } else {
+        if (this.options.testMode) {
+          if (callback)
+            callback(err, null)
+          else
+            return [err, null]
         }
+        if (!this.on) {
+          let ret = (dbg
+            + this.options.commentMark + this.options.reversibleCommentIndicator
+            + `${strippedLine}`)
+          if (callback)
+            callback(err, ret)
+          else
+            return [err, ret]
+        } else {
+          if (callback)
+            callback(err, (dbg + `${strippedLine}`))
+          else
+            return [err, (dbg + `${strippedLine}`)]
+        }
+      }
     }
-    line(line, callback = null) {
-        try {
-            this.linum++ // first line is line 1
-            //console.log(
-            //    `this.linenum=${this.linum}, this.stack.length=${this.stack.length}, line=${line}`)
-            let dbg = ""
-            if (this.debugOutput)
-                dbg = `[${this.onLinum}, ${this.linum}]`
-            let [isCmd, strippedLine, err] = this._parseLine_aux(line)
-            if (isCmd) {
-                if (this.options.testMode) {
-                    if (isCmd === 'start') {
-                        return [
-                            err,
-                            dbg = (this.on ? 'true,  ' : 'false, ')
-                            + `${strippedLine}`
-                        ]
-                    } else {
-                        return [err, null]
-                    }
-                } else {
-                    if (callback)
-                        callback(err, (dbg + `${strippedLine}`))
-                    else
-                        return [err, (dbg + `${strippedLine}`)]
-                }
-            } else {
-                if (this.options.testMode) {
-                    if (callback)
-                        callback(err, null)
-                    else
-                        return [err, null]
-                }
-                if (!this.on) {
-                    let ret = (dbg
-                        + this.options.commentMark + this.options.reversibleCommentIndicator
-                        + `${strippedLine}`)
-                    if (callback)
-                        callback(err, ret)
-                    else
-                        return [err, ret]
-                } else {
-                    if (callback)
-                        callback(err, (dbg + `${strippedLine}`))
-                    else
-                        return [err, (dbg + `${strippedLine}`)]
-                }
-            }
-        }
-        catch (e) {
-            if (e instanceof Error) {
-                if (callback)
-                    callback(e, null)
-                else
-                    return [e, null]
-            }
-            else {
-                if (callback)
-                    callback(new RppError("unknown error: " + e), null)
-                else
-                    return [new RppError("unknown error: " + e), null]
-            }
-        }
+    catch (e) {
+      if (e instanceof Error) {
+        if (callback)
+          callback(e, null)
+        else
+          return [e, null]
+      }
+      else {
+        if (callback)
+          callback(new RppError("unknown error: " + e), null)
+        else
+          return [new RppError("unknown error: " + e), null]
+      }
     }
+  }
+
+  _parseLine_aux(line) {
+    if (line.slice(0, 2) !== this.options.commentMark)
+      return [false, line, null]
+    let stripTarget = this.options.commentMark + this.options.reversibleCommentIndicator
+    // each line is always stripped of any previously inserted preproc comments
+    if (line.slice(0, stripTarget.length) === stripTarget) {
+      line = line.slice(stripTarget.length)
+      return [false, line, null]
+    }
+    let kind = { start: false, end: false }
+    let i0 = line.indexOf('if<<')
+    if (i0 == -1) {
+      i0 = line.indexOf('<<')
+      if (i0 == -1)
+        return [false, line, null]
+      i0 += 2
+      kind.end = true
+    } else {
+      i0 += 4
+      kind.start = true
+    }
+    let i1 = line.slice(i0).indexOf('>>')
+    if (i1 == -1)
+      return [false, line, null]
+    i1 += i0
+    let sub = line.slice(i0, i1)
+    if (kind.end) {
+      if (!this.stack.length) {
+        // too many end scope commnand - like unbalanced parentheses.
+        // Let's be strict.
+        let err = new RppError(`unexpected end directive line ${this.linum}, (unbalanced?)`)
+        return [true, line, err]
+      }
+      this.on = this.stack[this.stack.length - 1][0]
+      this.onLinum = this.stack[this.stack.length - 1][1]
+      this.stack.pop()
+      return ['end', line, null]
+    } else { // kind.start===true
+      if (!kind.start) throw new RppError("programming error")
+      let [isOn, err] = judgeLineArg(sub, this.definesJson, this.jsepPreprocInterpret)
+      this.stack.push([this.on, this.onLinum]) // save the previous state
+      this.on = isOn
+      this.onLinum = this.linum
+      return ['start', line, err]
+    }
+  }
+  line(line, callback = null) {
+    try {
+      this.linum++ // first line is line 1
+      //console.log(
+      //    `this.linenum=${this.linum}, this.stack.length=${this.stack.length}, line=${line}`)
+      let dbg = ""
+      if (this.debugOutput)
+        dbg = `[${this.onLinum}, ${this.linum}]`
+      let [isCmd, strippedLine, err] = this._parseLine_aux(line)
+      if (isCmd) {
+        if (this.options.testMode) {
+          if (isCmd === 'start') {
+            return [
+              err,
+              dbg = (this.on ? 'true,  ' : 'false, ')
+              + `${strippedLine}`
+            ]
+          } else {
+            return [err, null]
+          }
+        } else {
+          if (callback)
+            callback(err, (dbg + `${strippedLine}`))
+          else
+            return [err, (dbg + `${strippedLine}`)]
+        }
+      } else {
+        if (this.options.testMode) {
+          if (callback)
+            callback(err, null)
+          else
+            return [err, null]
+        }
+        if (!this.on) {
+          let ret = (dbg
+            + this.options.commentMark + this.options.reversibleCommentIndicator
+            + `${strippedLine}`)
+          if (callback)
+            callback(err, ret)
+          else
+            return [err, ret]
+        } else {
+          if (callback)
+            callback(err, (dbg + `${strippedLine}`))
+          else
+            return [err, (dbg + `${strippedLine}`)]
+        }
+      }
+    }
+    catch (e) {
+      if (e instanceof Error) {
+        if (callback)
+          callback(e, null)
+        else
+          return [e, null]
+      }
+      else {
+        if (callback)
+          callback(new RppError("unknown error: " + e), null)
+        else
+          return [new RppError("unknown error: " + e), null]
+      }
+    }
+  }
 }
 
 export default ReversiblePreproc
