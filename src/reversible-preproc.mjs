ca55b6052c1e08893723a60c5dfd93e23c37aef2
craigphicks <13205732+craigphicks@users.noreply.github.com>
2019-09-10 17:47:53 -0700

    backup - partway through refactoring for cmd multi input lines

--- src/reversible-preproc.mjs
+++ src/reversible-preproc.mjs
@@ -38,7 +38,7 @@ var defaultOptions = {
   // cmdStemMulti(StartEnd) used for 
   //    
   cmdStemMultiStart: '/*--',
-  cmdStemMultiEnd: '--*/',
+  cmdStemMultiEnd: '--end*/',
   cmdStem: '//--',
   cmdAddDef: 'addDef',
   cmdAddDefJson: 'addDefJson',
@@ -59,11 +59,75 @@ var defaultOptions = {
   annStem: '//!!',
 }
 
-function getLineHeadSymbol(line, opt, arrCmd, arrAnn) {
+const reservedIdentifiers = [
+  'true', 'false', 'null', 'undefined', 'def', 'ndef'
+]
+
+const symMultiIn = Symbol('multiIn')
+const symMultiInEnd = Symbol('multiInEnd')
+
+const symCmdAddDef = Symbol('cmdAddDef')
+const symCmdAddDefJson = Symbol('cmdAddDefJson')
+const symCmdAddDefEval = Symbol('cmdAddDefEval')
+const symCmdIf = Symbol('cmdIf')
+const symCmdElse = Symbol('cmdElse')
+const symCmdElif = Symbol('cmdElif')
+const symCmdEndif = Symbol('cmdEndif')
+const symCmdMacro = Symbol('cmdMacro')
+const symCmdTpl = Symbol('cmdTpl')
+const symCmdPartials = Symbol('cmdPartials')
+const symCmdRender = Symbol('cmdRender')
+const symCmdTplRender = Symbol('cmdTplRender')
+const symCmdIfTplRender = Symbol('cmdIfTplRender')
+
+const symAnnPlain = Symbol('annPlain')
+const symAnnRendered = Symbol('annRendered')
+const symAnnEndRendered = Symbol('annEndRendered')
+
+function isSubstrEqual(line, offset, str) {
+  return line.substr(offset, str.length) === str
+}
+
+
+function getLineHeadSymbol(line, opt, arrCmd, arrAnn, multiLineIn) {
   function testLineHead(line, start, str) {
-    return line.substr(start, str.length) === str
+    //return line.substr(start, str.length) === str
+    return isSubstrEqual(line, start, str)
   }
+  // input is a single line no EOL
   let wsOff = line.search(/\S/)
+  if (!multiLineIn.cmdSym) {
+    // scan first for multiline cmd start
+    if (testLineHead(line, wsOff, opt.cmdStemMultiStart)) {
+      let start = wsOff + opt.cmdStem.length
+      // check if cmdStemMultiEnd exists in same line
+      let substr = line.slice(start)
+      let idxEnd = line.substr(start).indexOf(opt.cmdStemMultiEnd)
+      if (idxEnd >= 0) {
+        substr.line.slice(start, idxEnd)
+      } else {
+        // end should come on a later line
+      }
+      for (let item of arrCmd) {
+        if (testLineHead(substr, 0, item[0]))
+          if (idxEnd > 0) {
+            // same as single line cmd
+            return [item[1], start + item[0].length + 1]
+          } else {
+            multiLineIn.cmdSym = item[1]
+            multiLineIn.lines.push([line, start + item[0].length + 1])
+            return [symMultiIn, null]
+          }
+      }
+      throw new RppError(`no known command found after stem in ${line}`)
+    } // if testLineHead(line, wsOff, opt.cmdStemMultiStart)
+  } else { // multiLineIn.cmdSym !== null
+    // do not check for line head command, only check for end.
+    let idxEnd = line.indexOf(opt.cmdStemMultiEnd)
+    // first add any data before end
+    multiLineIn.lines.push([line, 0, idxEnd]) // if 3rd el isn't present assume whole line
+    return [symMultiInEnd, null]
+  }
   if (wsOff == -1)
     return [null, null] // empty line 
   if (testLineHead(line, wsOff, opt.cmdStem)) {
@@ -121,27 +185,6 @@ function judgeLineArg(str, definesJson, jsepPreprocInterpret) {
 }
 
 
-const reservedIdentifiers = [
-  'true', 'false', 'null', 'undefined', 'def', 'ndef'
-]
-
-const symCmdAddDef = Symbol('cmdAddDef')
-const symCmdAddDefJson = Symbol('cmdAddDefJson')
-const symCmdAddDefEval = Symbol('cmdAddDefEval')
-const symCmdIf = Symbol('cmdIf')
-const symCmdElse = Symbol('cmdElse')
-const symCmdElif = Symbol('cmdElif')
-const symCmdEndif = Symbol('cmdEndif')
-const symCmdMacro = Symbol('cmdMacro')
-const symCmdTpl = Symbol('cmdTpl')
-const symCmdPartials = Symbol('cmdPartials')
-const symCmdRender = Symbol('cmdRender')
-const symCmdTplRender = Symbol('cmdTplRender')
-const symCmdIfTplRender = Symbol('cmdIfTplRender')
-
-const symAnnPlain = Symbol('annPlain')
-const symAnnRendered = Symbol('annRendered')
-const symAnnEndRendered = Symbol('annEndRendered')
 
 
 class ReversiblePreproc {
@@ -178,6 +221,10 @@ class ReversiblePreproc {
       renderedOn: false, // currently in an annotated rendered region, (will be cleared, maybe rerendered) 
       tplStringArr: [], // cmd-tpl lines are accumulated until cmd-render is encountered, thenoutput 
       tplPartials: {},
+      multiLineIn: {
+        cmdSym: null,
+        lines: []
+      }
     }
     this.cmdsSorted = [
       [options.cmdAddDef, symCmdAddDef],
@@ -340,27 +387,6 @@ class ReversiblePreproc {
         this.parseState.tplStringArr = []
         this.parseState.tplPartials = {}
         return [true, lineArr]
-
-        // let pre = this.options.annStem + this.options.annRendered
-        // let post = this.options.annStem + this.options.annEndRendered
-        // let res = Mustache.render(
-        //     this.parseState.tplStringArr.join(" "),
-        //     this.definesJson,
-        //     //          this.parseState.tplPartials
-        //   )
-        // if (Reflect.ownKeys(this.parseState.tplPartials).length > 0) {
-        //   res = Mustache.render(
-        //     res, this.definesJson,
-        //     this.parseState.tplPartials
-        //   )
-        // }
-        // this.parseState.tplStringArr = []
-        // this.parseState.tplPartials = {}
-        // let lineArr = [line]
-        //   .concat([pre])
-        //   .concat(res.split('\n'))
-        //   .concat([post])
-        // return [true, lineArr]
       }
       else
         throw new RppError(`found ${sym.toString()}, expecting symCmdTpl or symCmdRender`)
@@ -405,27 +431,127 @@ class ReversiblePreproc {
     // TODO cases symCmdElif, symCmdElse, symCmdTplRender, symCmdIfTplRender
   }
 
-  _ensureEol(line){
-    _assert(typeof line ==='string')
-    if (line.length < this._eol().length 
-    || line.slice(-1*this._eol().length)!==this._eol()){
-      return line+this._eol()
+  _ensureEol(line) {
+    _assert(typeof line === 'string')
+    if (line.length < this._eol().length
+      || line.slice(-1 * this._eol().length) !== this._eol()) {
+      return line + this._eol()
     }
     return line
   }
-  _removeAnyEol(line){
-    if (line.length>=2 && line.slice(-2)==='\r\n')
-      return line.slice(0,-2)
-    if (line.length>=1 && line.slice(-1)==='\n')
-      return line.slice(0,-1)
+  _removeAnyEol(line) {
+    if (line.length >= 2 && line.slice(-2) === '\r\n')
+      return line.slice(0, -2)
+    if (line.length >= 1 && line.slice(-1) === '\n')
+      return line.slice(0, -1)
     return line
   }
+
+  _whichCmd(line, offset) {
+    for (let item of this.parseState.cmdsSorted) {
+      if (isSubstrEqual(line, offset, item[0]))
+        return item
+    }
+    throw new RppError(`no command found in ${line} offset ${offset}`)
+  }
+
   line(line, pushOut = null, callback = (e, x) => { return [e, x] }) {
+    try {
+      line = this._removeAnyEol(line) // in case input EOL !== output EOL
+      this.parseState.linum++ // first line is line 1
+      let wsOff = line.search(/\S/)
+
+      // should we search for line head?
+      if (!this.parseState.multiLine.cmdSym) {
+        if (wsOff >= 0 && isSubstrEqual(line, wsOff, this.options.cmdStemMultiStart)) {
+          // begin multiIn
+          let cmdline = line.slice(wsOff + this.options.cmdStemMultiStart.length)
+          let cmdlineStart = wsOff + this.options.cmdStemMultiStart.length
+
+          let haveEnd = false
+          let idxEnd = cmdline.indexOf(this.options.cmdStemMultiEnd)
+          if (idxEnd >= 0) {
+            cmdline = cmdline.slice(0, idxEnd)
+            haveEnd = true
+          } else {
+            idxEnd = cmdline.length
+          }
+          let item = this._whichCmd(cmdline, 0) // throws if no cmd found
+          this.parseState.multiLine.cmdSym = item[1]
+          this.parseState.multiLine.lines.push(
+            [
+              line,
+              cmdlineStart + item[0] + 1,
+              cmdlineStart + idxEnd
+            ]
+          )
+          if (!haveEnd)
+            return callback(null, null)
+        } else if (wsOff >= 0 && isSubstrEqual(line, wsOff, this.options.cmdStem)) {
+          let cmdline = line.slice(wsOff + this.options.cmdStem.length)
+          let cmdlineStart = wsOff + this.options.cmdStem.length
+          let item = this._whichCmd(cmdline, 0) // throws if no cmd found
+          this.parseState.multiLine.cmdSym = item[1]
+          this.parseState.multiLine.lines.push(
+            [
+              line,
+              cmdlineStart + item[0] + 1,
+              line.length
+            ]
+          )
+          this.parseState.multiLine.endDetected = true
+        }
+      } else { // this.parseState.multiLine.cmdSym is not null
+        // multiline input continuation
+        let idxEnd = line.length
+        //let cmdline = line
+        let haveEnd = false
+        if (wsOff >= 0) {
+          let tmp = line.substr(wsOff).indexOf(this.options.cmdStemMultiEnd)
+          if (tmp >= 0) {
+            idxEnd = wsOff + tmp
+            haveEnd = true
+          }
+        }
+        this.parseState.multiLine.lines.push([line, 0, idxEnd])
+        this.parseStart.multiLine.endDetected = haveEnd
+        if (!haveEnd) {
+          return callback(null, null)
+        }
+      }
+      if (this.parseStart.multiLine.endDetected) {
+        // process accumulated command lines and return result
+        let lineOutArr = _processMultiLineCmd()
+        _assert(pushOut || lineOutArr.length <= 1)
+        if (!pushOut) {
+          // older tests need back compat
+          let lineOut = lineOutArr.length ? this._ensureEol(lineOutArr[0]) : null
+          return callback(null, lineOut)
+        } else {
+          for (let lineOut of lineOutArr) {
+            if (lineOut)
+              pushOut(this._ensureEol(lineOut))
+          }
+          return callback(null, null)
+        }
+      } // if (this.parseStart.multiLine.endDetected)
+    } // try
+    catch (e) {
+      if (e instanceof Error) {
+        return callback(e, null)
+      } else {
+        return callback(new RppError("unknown error: " + e), null)
+      }
+    }
+  } // line
+
+  lineX(line, pushOut = null, callback = (e, x) => { return [e, x] }) {
     try {
       line = this._removeAnyEol(line) // in case input EOL !== output EOL
       this.parseState.linum++ // first line is line 1
       let callbackLineout = null
       let [multi, strippedLine] = this._parseLine_aux2(line)
+
       if (!multi) {
         let lineOut = this._ensureEol(strippedLine)
         if (pushOut)
@@ -438,7 +564,7 @@ class ReversiblePreproc {
           throw RppError
             // eslint-disable-next-line no-unexpected-multiline
             ('pushOut must be defined to enable multiple line output (i.e. templates)')
-        for (let item of strippedLine){
+        for (let item of strippedLine) {
           let lineOut = this._ensureEol(item)
           pushOut(lineOut)
         }
